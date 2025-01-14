e9aac9c3ec04e3c9692979fd71f7ed0f8e924ea9
alvacode <alvaro89_piura@hotmail.com>
2025-01-14 13:04:04 -0500

    primer cambio de la pantalla de perfil

--- src/components/elementos/topbarDashboard.js
+++ src/components/elementos/topbarDashboard.js
@@ -2,30 +2,37 @@
 import Image from 'next/image'
 import React, { useEffect, useState } from 'react'
 import { handleGetCookie } from '../serverComp/addCookie';
-
+import PersonIcon from '@mui/icons-material/Person';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import NotificationsIcon from '@mui/icons-material/Notifications';
+import PowerSettingsNewIcon from '@mui/icons-material/PowerSettingsNew';
 import { getSession, logout } from '@/authentication/lib';
 import { Button } from '../ui/button';
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from '../ui/dropdown-menu';
+import { useRouter } from 'next/navigation';
 
 const URL_IMG_UDEP = "https://res.cloudinary.com/ddcb3fk7s/image/upload/v1723319328/udep_logo_eqcizp.png";
 const URL_IMG_LABSAC = "https://res.cloudinary.com/ddcb3fk7s/image/upload/v1723319348/logo_lab_sac_guinda_xefpo1.png";
+const URL_RECORDS = process.env.NEXT_PUBLIC_URL_RECORDS
 
-export default function TopBarDashboard() {
+export default function TopBarDashboard({historial}) {
+    const router = useRouter();
     const [userData, setUserData] = useState(null);
     useEffect(()=>{
         async function fetchCookie() {
             const session = await getSession();
             const values = session?.user;
-            
-            setUserData(values)
+            setUserData(values);
         }
         fetchCookie();
     },[]);
     const handleClickLogout=async()=>{
         await logout();
     }
+    const handleClickPerfil=async()=>{
+        router.push("/dashboard/user")
+    }
+
   return (
     <div className='h-24 w-full bg-white shadow-sm flex flex-row items-center py-4 justify-between z-10'>
         <div className='ml-4 flex flex-row px-4'>
@@ -59,13 +66,25 @@ export default function TopBarDashboard() {
                     <DropdownMenuLabel>
                         Mi cuenta
                     </DropdownMenuLabel>
+                   
                     <DropdownMenuSeparator/>
+                    <DropdownMenuItem >
+                        <Button
+                            variant="ghost"
+                            className="flex items-center justify-center"
+                            onClick={handleClickPerfil}
+                        >
+                            <p><PersonIcon/><span className='ml-2'>Ver perfil</span></p>
+                        </Button>
+                    </DropdownMenuItem>
                     <DropdownMenuItem>
                         <Button 
+                            className="flex items-center justify-center"
+
                             variant="ghost"
                             onClick={handleClickLogout}
                         >
-                            <span>Cerrar Sesion</span>
+                            <p><PowerSettingsNewIcon/><span className='ml-2'>Cerrar Sesion</span></p>
                         </Button>
                     </DropdownMenuItem>
                 </DropdownMenuContent>
@@ -79,3 +98,19 @@ export default function TopBarDashboard() {
     </div>
   )
 };
+
+
+export async function getServerSideProps() {
+    const res = await fetch(URL_RECORDS,{
+        headers : {
+            'Content-Type' : 'application/json',
+
+        }
+    });
+    const historial = await res.json();
+    return {
+        props : {
+            historial
+        }
+    }
+}
