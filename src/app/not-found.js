3f62f9cd488464231e28ead8c4049056db23bf99
alvacode <alvaro89_piura@hotmail.com>
2025-02-03 12:05:21 -0500

    pagina no encontrada

---
+++ src/app/not-found.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Link from 'next/link';
+
+export default function NotFound() {
+  return (
+    <div className='w-full bg-white h-screen flex flex-col items-center justify-center'>
+        <h2 className='text-orange-500'>Pagina no encontrada</h2>
+        <Link href={"/"} className='text-black'>Regresar al inicio</Link>
+    </div>
+  )
+}
--- src/components/elementos/topbarDashboard.js
+++ src/components/elementos/topbarDashboard.js
@@ -1,30 +1,27 @@
 "use client"
 import Image from 'next/image'
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import PersonIcon from '@mui/icons-material/Person';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import NotificationsIcon from '@mui/icons-material/Notifications';
 import PowerSettingsNewIcon from '@mui/icons-material/PowerSettingsNew';
-import { getSession, logout } from '@/authentication/lib';
+import { logout } from '@/authentication/lib';
 import { Button } from '../ui/button';
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from '../ui/dropdown-menu';
 import { useRouter } from 'next/navigation';
+import { useFetch, useSession } from '../hooks/customHooks';
+import { Loader2 } from 'lucide-react';
 
 const URL_IMG_UDEP = "https://res.cloudinary.com/ddcb3fk7s/image/upload/v1723319328/udep_logo_eqcizp.png";
 const URL_IMG_LABSAC = "https://res.cloudinary.com/ddcb3fk7s/image/upload/v1723319348/logo_lab_sac_guinda_xefpo1.png";
-const URL_RECORDS = process.env.NEXT_PUBLIC_URL_RECORDS
+const URL_RECORDS = process.env.NEXT_PUBLIC_URL_RECORDS;
 
 export default function TopBarDashboard() {
     const router = useRouter();
-    const [userData, setUserData] = useState(null);
+    const {loading : loadingDataRecords, dataResponse : dataRecords } = useFetch(URL_RECORDS);
+    const {loading : loadingDataSession , dataSession} = useSession();
+    console.log(dataRecords);
     
-    useEffect(()=>{
-        async function fetchCookie() {
-            const session = await getSession();
-            const values = session?.user;
-            setUserData(values);
-        }
-        fetchCookie();
-    },[]);
 
     const handleClickLogout=async()=>{
         await logout();
@@ -53,45 +50,80 @@ export default function TopBarDashboard() {
             />
         </div>
         <div className='flex flex-row items-center px-4 mr-8'>
-            <DropdownMenu>
-                <DropdownMenuTrigger asChild>
-                <Button 
-                    className=' flex flex-row items-center py-6 rounded-2xl text-white bg-guindaOpaco mr-4 hover:text-guinda border hover:border-guinda'
-                    variant="ghost"
-                    >
-                    <AccountCircleIcon />
-                    <p className=' ml-4 hidden lg:block'>{userData&& userData.username}</p>
-                </Button>
-                </DropdownMenuTrigger>
-                <DropdownMenuContent>
-                    <DropdownMenuLabel>
-                        Mi cuenta
-                    </DropdownMenuLabel>
-                   
-                    <DropdownMenuSeparator/>
-                    <DropdownMenuItem >
-                        <Button
-                            variant="ghost"
-                            className="flex items-center justify-center"
-                            onClick={handleClickPerfil}
-                        >
-                            <p><PersonIcon/><span className='ml-2'>Ver perfil</span></p>
-                        </Button>
-                    </DropdownMenuItem>
-                    <DropdownMenuItem>
-                        <Button 
-                            className="flex items-center justify-center"
-
-                            variant="ghost"
-                            onClick={handleClickLogout}
-                        >
-                            <p><PowerSettingsNewIcon/><span className='ml-2'>Cerrar Sesion</span></p>
-                        </Button>
-                    </DropdownMenuItem>
-                </DropdownMenuContent>
-            </DropdownMenu>
-            <div>
+           {
+            loadingDataSession ? 
+            <Button
+            className=' flex flex-row items-center py-6 rounded-2xl text-white bg-guindaOpaco mr-4 hover:text-guinda border hover:border-guinda'
+            
+            >
+                <Loader2 className='mr-2 h-4 w-4 animate-spin' />
+            </Button> : 
+             <DropdownMenu>
+             <DropdownMenuTrigger asChild>
+             <Button 
+                 className=' flex flex-row items-center py-6 rounded-2xl text-white bg-guindaOpaco mr-4 hover:text-guinda border hover:border-guinda'
+                 variant="ghost"
+                 >
+                 <AccountCircleIcon />
+                 <p className=' ml-4 hidden lg:block'>{dataSession&& dataSession.sub}</p>
+             </Button>
+             </DropdownMenuTrigger>
+             <DropdownMenuContent>
+                 <DropdownMenuLabel>
+                     Mi cuenta
+                 </DropdownMenuLabel>
                 
+                 <DropdownMenuSeparator/>
+                 <DropdownMenuItem >
+                     <Button
+                         variant="ghost"
+                         className="flex items-center justify-center"
+                         onClick={handleClickPerfil}
+                     >
+                         <p><PersonIcon/><span className='ml-2'>Ver perfil</span></p>
+                     </Button>
+                 </DropdownMenuItem>
+                 <DropdownMenuItem>
+                     <Button 
+                         className="flex items-center justify-center"
+
+                         variant="ghost"
+                         onClick={handleClickLogout}
+                     >
+                         <p><PowerSettingsNewIcon/><span className='ml-2'>Cerrar Sesion</span></p>
+                     </Button>
+                 </DropdownMenuItem>
+             </DropdownMenuContent>
+         </DropdownMenu>
+           }
+            <div>
+                {
+                    (!loadingDataRecords && dataRecords) &&
+                    <DropdownMenu>
+                        <DropdownMenuTrigger
+                            asChild
+                        >
+                            <Button
+                                variant="ghost"
+                                className="rounded-full text-guinda"
+                            >
+                                <NotificationsIcon/>
+                            </Button>
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent>
+                            {
+                                dataRecords && 
+                                dataRecords?.data.map((item, key)=>
+                                    key < 5 &&
+                                    (
+                                    <div className='p-4'>
+                                        <p>{item?.user?.userName}</p>
+                                    </div>
+                                ))
+                            }
+                        </DropdownMenuContent>
+                    </DropdownMenu>
+                }
             </div>
         </div>
     </div>
